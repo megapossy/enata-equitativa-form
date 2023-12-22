e3be6e081cdae0708001e3a77dbdc12cdb4dd124
Tawing <yabuking84@gmail.com>
2023-12-22 09:32:27 +0400

    update

--- server/plugins/insertSelectBox.ts
+++ server/plugins/insertSelectBox.ts
@@ -1,6 +1,5 @@
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook("render:html", (html, { event }) => {
-    console.log('XXXXXXXXXXXXXx')
     html.bodyAppend.push("<div id='select-box' />");
   });
 });
